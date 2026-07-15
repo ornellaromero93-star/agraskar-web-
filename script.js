52726d9d49b522383c68976342d7f1a5e99eb589
ornellaromero93-star <ornellaromero93@gmail.com>
2026-07-14 22:42:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -59,25 +59,7 @@ if(boton){
     }
 
 }
-// SCROLL SUAVE
 
-document.querySelectorAll('a[href^="#"]').forEach(link=>{
-
-    link.addEventListener("click",function(e){
-
-        e.preventDefault();
-
-        const destino=document.querySelector(this.getAttribute("href"));
-
-        destino.scrollIntoView({
-
-            behavior:"smooth"
-
-        });
-
-    });
-
-});
 /*=========================================
         PESTAÑAS DE JUEGOS
 =========================================*/
